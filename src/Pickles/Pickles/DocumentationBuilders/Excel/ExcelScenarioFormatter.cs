7fea69f95911df58aaf628d64207e131ff05055e
Jeffrey Cameron <jeffreycameron@gmail.com>
2012-05-09 20:39:22 -0400

    Cleaned up some using statements

--- src/Pickles/Pickles/DocumentationBuilders/Excel/ExcelScenarioFormatter.cs
+++ src/Pickles/Pickles/DocumentationBuilders/Excel/ExcelScenarioFormatter.cs
@@ -18,12 +18,6 @@
 
 #endregion
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Pickles.Parser;
-using DocumentFormat.OpenXml.Spreadsheet;
 using ClosedXML.Excel;
 
 namespace Pickles.DocumentationBuilders.Excel
